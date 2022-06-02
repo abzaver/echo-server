a4a339805c2465884ea1a7a6036987801f32815c
abzaver <abzaver@gmail.com>
2022-06-02 21:46:13 +0200

    RabbitMQ different messages examples added

--- rabbitMQ/consumer/consumer.go
+++ rabbitMQ/consumer/consumer.go
@@ -1,51 +1,100 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"log"
 
 	"github.com/streadway/amqp"
 )
 
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
 func main() {
-	fmt.Println("RabbitMQ in Golang: Getting started tutorial")
+	// Connecting to RabbitMQ server
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
 
-	connection, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
-	if err != nil {
-		panic(err)
-	}
-	defer connection.Close()
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
 
-	fmt.Println("Successfully connected to RabbitMQ instance")
-
-	// opening a channel over the connection established to interact with RabbitMQ
-	channel, err := connection.Channel()
-	if err != nil {
-		panic(err)
-	}
-	defer channel.Close()
-
-	// declaring consumer with its properties over channel opened
-	msgs, err := channel.Consume(
-		"testing", // queue
-		"",        // consumer
-		true,      // auto ack
-		false,     // exclusive
-		false,     // no local
-		false,     // no wait
-		nil,       //args
+	// Declaring the queue (ensure it matches the producer's configuration)
+	queue, err := ch.QueueDeclare(
+		"example_queue", // queue name
+		true,            // durable
+		false,           // delete when unused
+		false,           // exclusive
+		false,           // no-wait
+		nil,             // arguments
 	)
-	if err != nil {
-		panic(err)
-	}
+	failOnError(err, "Failed to declare a queue")
 
-	// print consumed messages from queue
+	// Registering the consumer
+	msgs, err := ch.Consume(
+		queue.Name, // queue name
+		"",         // consumer tag
+		true,       // auto-ack
+		false,      // exclusive
+		false,      // no-local
+		false,      // no-wait
+		nil,        // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	// Creating a channel to wait for incoming messages
 	forever := make(chan bool)
+
+	// Processing the received messages
 	go func() {
-		for msg := range msgs {
-			fmt.Printf("Received Message: %s\n", msg.Body)
+		for d := range msgs {
+			switch d.ContentType {
+			case "application/json":
+				handleJSONMessage(d.Body)
+			case "application/xml":
+				handleXMLMessage(d.Body)
+			case "text/plain":
+				handleTextMessage(d.Body)
+			case "application/octet-stream":
+				handleByteArrayMessage(d.Body)
+			default:
+				log.Printf("Received unknown content type: %s", d.ContentType)
+			}
 		}
 	}()
 
-	fmt.Println("Waiting for messages...")
+	log.Println("Waiting for messages. To exit press CTRL+C")
 	<-forever
+}
+
+// handleJSONMessage processes JSON messages
+func handleJSONMessage(body []byte) {
+	var message map[string]string
+	err := json.Unmarshal(body, &message)
+	if err != nil {
+		log.Printf("Failed to unmarshal JSON: %s", err)
+		return
+	}
+	log.Printf("Received JSON message: %v", message)
+}
+
+// handleXMLMessage processes XML messages
+func handleXMLMessage(body []byte) {
+	xmlData := string(body)
+	log.Printf("Received XML message: %s", xmlData)
+}
+
+// handleTextMessage processes plain text messages
+func handleTextMessage(body []byte) {
+	text := string(body)
+	log.Printf("Received text message: %s", text)
+}
+
+// handleByteArrayMessage processes byte array messages
+func handleByteArrayMessage(body []byte) {
+	log.Printf("Received byte array message: %v", body)
 }
--- rabbitMQ/publisher/publisher.go
+++ rabbitMQ/publisher/publisher.go
@@ -1,58 +1,91 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"log"
 
 	"github.com/streadway/amqp"
 )
 
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
 func main() {
+	// Connecting to RabbitMQ server
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
 
-	connection, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
-	if err != nil {
-		panic(err)
-	}
-	defer connection.Close()
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
 
-	fmt.Println("Successfully connected to RabbitMQ instance")
-
-	// opening a channel over the connection established to interact with RabbitMQ
-	channel, err := connection.Channel()
-	if err != nil {
-		panic(err)
-	}
-	defer channel.Close()
-
-	// declaring queue with its properties over the the channel opened
-	queue, err := channel.QueueDeclare(
-		"testing", // name
-		false,     // durable
-		false,     // auto delete
-		false,     // exclusive
-		false,     // no wait
-		nil,       // args
+	// Declaring a queue
+	queue, err := ch.QueueDeclare(
+		"example_queue", // queue name
+		true,            // durable
+		false,           // delete when unused
+		false,           // exclusive
+		false,           // no-wait
+		nil,             // arguments
 	)
-	if err != nil {
-		panic(err)
-	}
+	failOnError(err, "Failed to declare a queue")
 
-	// publishing a message
-	err = channel.Publish(
-		"",        // exchange
-		"testing", // key
-		false,     // mandatory
-		false,     // immediate
+	// Sending a JSON message
+	jsonData, err := json.Marshal(map[string]string{"key": "value"})
+	failOnError(err, "Failed to marshal JSON")
+	err = ch.Publish(
+		"",         // exchange
+		queue.Name, // routing key
+		false,      // mandatory
+		false,      // immediate
+		amqp.Publishing{
+			ContentType: "application/json",
+			Body:        jsonData,
+		})
+	failOnError(err, "Failed to publish JSON message")
+
+	// Sending an XML message
+	xmlData := `<example><key>value</key></example>`
+	err = ch.Publish(
+		"",         // exchange
+		queue.Name, // routing key
+		false,      // mandatory
+		false,      // immediate
+		amqp.Publishing{
+			ContentType: "application/xml",
+			Body:        []byte(xmlData),
+		})
+	failOnError(err, "Failed to publish XML message")
+
+	// Sending a plain text message
+	textData := "This is a plain text message"
+	err = ch.Publish(
+		"",         // exchange
+		queue.Name, // routing key
+		false,      // mandatory
+		false,      // immediate
 		amqp.Publishing{
 			ContentType: "text/plain",
-			Body:        []byte("Test Message"),
-		},
-	)
+			Body:        []byte(textData),
+		})
+	failOnError(err, "Failed to publish text message")
 
-	if err != nil {
-		panic(err)
-	}
+	// Sending a byte array message
+	byteArrayData := []byte{0x00, 0x01, 0x02, 0x03, 0x04}
+	err = ch.Publish(
+		"",         // exchange
+		queue.Name, // routing key
+		false,      // mandatory
+		false,      // immediate
+		amqp.Publishing{
+			ContentType: "application/octet-stream",
+			Body:        byteArrayData,
+		})
+	failOnError(err, "Failed to publish byte array message")
 
-	fmt.Println("Queue status:", queue)
-
-	fmt.Println("Successfully published message")
+	log.Println("All messages were successfully published")
 }
